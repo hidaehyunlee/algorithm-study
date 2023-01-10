a1b0d20c569cca7454c67fbcbe38f9890bc6ee44
Daehyun Lee <leehi0210@gmail.com>
2023-01-10 15:49:18 +0900

    02_greedy

---
+++ 02_Greedy/01_거스름돈.py
@@ -0,0 +1,14 @@
+def solution(n):
+    change = [500, 100, 50, 10]
+    count = 0
+
+    for coin in change:
+        count += n // coin
+        n %= coin
+
+    print(count)
+
+n = input(int())
+solution(n)
+ 
+    
