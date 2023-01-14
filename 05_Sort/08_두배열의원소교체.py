5191d49b266ef178292fb12c591dfe218bf75856
Daehyun Lee <leehi0210@gmail.com>
2023-01-14 13:32:03 +0900

    05_sort/08 오답 정정

--- 05_Sort/08_두배열의원소교체.py
+++ 05_Sort/08_두배열의원소교체.py
@@ -3,7 +3,9 @@ n, k = map(int, input().split())
 a = sorted(list(map(int, input().split()))) # 오름
 b = sorted(list(map(int, input().split())), reverse=True) # 내림
 
+# 오답: b가 더 작을 수도 있으니 비교해야함
 for i in range(k):
-    a[i] = b[i]
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
 
 print(sum(a))
