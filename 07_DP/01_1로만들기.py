cb62ddbaa83e6db71c8ab0862e888a369ccd44a2
Daehyun Lee <leehi0210@gmail.com>
2023-02-01 20:41:11 +0900

    [1] 07_DP 복습

--- 07_DP/01_1로만들기.py
+++ 07_DP/01_1로만들기.py
@@ -1,18 +1,35 @@
-# p.217
+# p.217 1로 만들기
 
-n = int(input())
+# 두번째 풀이
+# 점화식: 각 4개의 연산이 최소가 되면됨
+x = int(input())
 d = [0] * 30001
 
-# bottom-up (loop)
-for i in range(2, n + 1):
-    d[i] = d[i - 1] + 1
-    
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i // 2] + 1)
+for i in range(2, x + 1):
+    # 1을 빼고 시작하는 이유는 다음에 계산할 나누기가 1을 뺀 값보다 작거나 큼에 따라 어차피 교체되기 때문
+    d[i] = d[i - 1] + 1 # + 1은 cnt용
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i // 5] + 1) # 여기서 d[x]의 값이 교체됨 
     if i % 3 == 0:
         d[i] = min(d[i], d[i // 3] + 1)
-    if i % 5 == 0:
-        d[i] = min(d[i], d[i // 5] + 1)
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+    
+print(d[x])
 
-print(d[n])
+# n = int(input())
+# d = [0] * 30001
+
+# # bottom-up (loop)
+# for i in range(2, n + 1):
+#     d[i] = d[i - 1] + 1
+    
+#     if i % 2 == 0:
+#         d[i] = min(d[i], d[i // 2] + 1)
+#     if i % 3 == 0:
+#         d[i] = min(d[i], d[i // 3] + 1)
+#     if i % 5 == 0:
+#         d[i] = min(d[i], d[i // 5] + 1)
+
+# print(d[n])
 
--- 07_DP/02_개미전사.py
+++ 07_DP/02_개미전사.py
@@ -1,13 +1,33 @@
 # p.220 개미전사
 # 풀이: 점화식: a_i = max(a_i-1, a_i-2 + k_i)
 
-n = int(input())
-arr = list(map(int, input().split()))
+# 두번째 풀이
+# 1 3 1 5
+# 1, 3, 1, 5, 1 1, 1 5, 3 5, 0 -> 경우의 수 8개
+#   -> i - 2 에서 식량을 털면 현재 i에서 털 수 있음
+#   -> i - 1 에서 식량을 털면 현재 i에서 x
+#   -> 둘 중 어느 값이 더 큰가 비교하면 됨
+#       -> 점화식: d[i] = max(d[i - 1], d[i - 2] + k[i])
+
+n = int(input()) # 식량창고 (3<= n <=100)
+k = list(map(int, input().split())) # 각 식량창고에 저장된 식량 수 (0<= k <=1000)
+
 d = [0] * 100
+d[0] = k[0]
+d[1] = max(k[0], k[1])
 
-d[0] = arr[0]
-d[1] = max(arr[0], arr[1])
 for i in range(2, n):
-    d[i] = max(d[i - 1], d[i - 2] + arr[i])
+    d[i] = max(d[i-1], d[i-2] + k[i])
 
-print(d[n - 1])
+print(d[n - 1]) # d[n]은 indexError
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+# d = [0] * 100
+
+# d[0] = arr[0]
+# d[1] = max(arr[0], arr[1])
+# for i in range(2, n):
+#     d[i] = max(d[i - 1], d[i - 2] + arr[i])
+
+# print(d[n - 1])
